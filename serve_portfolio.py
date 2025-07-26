3cffd570e8a15ff3e52bc19fc23686adcab9731f
mitchyc24 <mitchell.carroll24@gmail.com>
2025-07-26 09:17:02 -0400

    chore: Remove outdated comments regarding static file directory

--- serve_portfolio.py
+++ serve_portfolio.py
@@ -2,9 +2,6 @@ import uvicorn
 from starlette.applications import Starlette
 from starlette.staticfiles import StaticFiles
 
-# Assumes your static files (like index.html) are in the same directory
-# as this script. If they are in a subdirectory (e.g., "_site"),
-# change `directory` to `directory="_site"`.
 app = Starlette(debug=True)
 app.mount('/', StaticFiles(directory='.', html=True), name='static')
 
