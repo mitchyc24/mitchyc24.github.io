ed1dce0bd91960646ca6fd4f9edbb08913d0c038
Mitchell Carroll <mitchell.carroll24@gmail.com>
2025-10-17 21:36:36 -0400

    Refactor code into Progressive Web App

---
+++ sw.js
@@ -0,0 +1,70 @@
+const CACHE_VERSION = 'todo-app-v1';
+const APP_CACHE = `carroll-compute-${CACHE_VERSION}`;
+const NAVIGATION_FALLBACK = './todo.html';
+const ASSETS = [
+  NAVIGATION_FALLBACK,
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(APP_CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== APP_CACHE).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+
+  if (request.method !== 'GET') {
+    return;
+  }
+
+  const url = new URL(request.url);
+  const isSameOrigin = url.origin === self.location.origin;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(APP_CACHE).then((cache) => cache.put(request, copy));
+          return response;
+        })
+        .catch(() => caches.match(request).then((cached) => cached || caches.match(NAVIGATION_FALLBACK)))
+    );
+    return;
+  }
+
+  if (!isSameOrigin) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(request).then((networkResponse) => {
+        if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+          return networkResponse;
+        }
+
+        const copy = networkResponse.clone();
+        caches.open(APP_CACHE).then((cache) => cache.put(request, copy));
+        return networkResponse;
+      });
+    })
+  );
+});
